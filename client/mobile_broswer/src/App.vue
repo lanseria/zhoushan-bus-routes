5877b702e6c1dc707591a45ce3e38e7e8c23bbca
lanseria <zhangchao564265135@hotmail.com>
2018-03-26 22:49:11 +0800

    still use styl to process styleSheet

---
+++ client/mobile_broswer/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <m-header class="sub-wrapper"></m-header>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import MHeader from '@/components/m-header/m-header'
+export default {
+  name: 'App',
+  mounted () {
+    document.addEventListener('touchmove', e => e.preventDefault(), {passive: false})
+  },
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-overflow-scrolling: touch;
+}
+.sub-wrapper {
+  height: 100%;
+  overflow: auto;
+}
+</style>
---
+++ client/mobile_broswer/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App'
+import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+import router from './router'
+import '@/common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('@/common/image/default.png')
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
