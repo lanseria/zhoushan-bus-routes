d83f5103356e5cbebeeaeea714b8f4b60f50d2b1
Lanseria <zhangchao564265135@hotmail.com>
2018-04-01 23:57:48 +0800

    remove user tag

--- client/mobile_broswer/src/components/m-header/m-header.vue
+++ client/mobile_broswer/src/components/m-header/m-header.vue
@@ -2,9 +2,9 @@
   <div class="m-header">
     <div class="icon"></div>
     <h1 class="text" @click="redirect" v-text="projectNameAndVersion"></h1>
-    <router-link to="/user" class="mine" tag="div">
+    <!-- <router-link to="/user" class="mine" tag="div">
       <i class="icon-mine"></i>
-    </router-link>
+    </router-link> -->
     <confirm ref="github" :text="text" :confirmBtnText="confirmBtnText" @confirm="linkto"></confirm>
   </div>
 </template>
