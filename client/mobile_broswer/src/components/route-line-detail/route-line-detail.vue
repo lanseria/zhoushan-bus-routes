45f0d3ed0309982556888801c22ca366e85c9015
Lanseria <zhangchao564265135@hotmail.com>
2018-04-02 12:14:59 +0800

    修改了版本号写法，弹出效果改变

--- client/mobile_broswer/src/components/route-line-detail/route-line-detail.vue
+++ client/mobile_broswer/src/components/route-line-detail/route-line-detail.vue
@@ -4,23 +4,31 @@
       <div class="back"><i class="icon-corner-down-left" @click="back"></i></div>
       <h1 class="title" v-text="id"></h1>
       <div class="start-or-end-wrapper" v-if="nameOfStartAndEnd">
-        <div class="start" v-text="`起始站：${nameOfStartAndEnd.start}`"></div>
-        <div class="btn-translate" @click="handleTransRoute">
-          <i class="icon-random icon"></i>返程</div>
-        <div class="start" v-text="`终点站：${nameOfStartAndEnd.end}`"></div>
-      </div>
-      <transition name="slide">
-        <div v-show="timeMsg" class="notify" v-html="timeMsg"></div>
-      </transition>
-      <scroll v-if="currentRouteStations" :data="currentRouteStations" class="list" ref="list">
-        <div class="all-stations">
-          <div class="one-stataion" v-for="(item, index) in currentRouteStations" :key="index" @click="handleGetThisStationDetail(index)">
-            <img class="avatar" :src="item.avatar" />
-            <span class="name">{{item.name}}</span>
-            <div class="detail">此站等候</div>
-          </div>
+        <div class="start">
+          <span class="tag">起</span>
+          <span class="point">{{nameOfStartAndEnd.start}}</span>
         </div>
-      </scroll>
+        <div class="btn-translate" @click="handleTransRoute">
+          <i class="icon-repeat icon"></i>返程</div>
+        <div class="end">
+          <span class="tag">终</span>
+          <span class="point">{{nameOfStartAndEnd.end}}</span>
+        </div>
+      </div>
+      <div class="content-wrapper">
+        <transition name="slidedown">
+          <div v-show="timeMsg" class="notify" v-html="timeMsg"></div>
+        </transition>
+        <scroll v-if="currentRouteStations" :data="currentRouteStations" class="list" ref="list">
+          <div class="all-stations">
+            <div class="one-stataion" v-for="(item, index) in currentRouteStations" :key="index" @click="handleGetThisStationDetail(index)">
+              <img class="avatar" :src="item.avatar" />
+              <span class="name">{{item.name}}</span>
+              <div class="detail">此站等候</div>
+            </div>
+          </div>
+        </scroll>
+      </div>
     </div>
   </transition>
 </template>
@@ -127,19 +135,26 @@ export default {
     transition: all 0.3s
   .slide-enter,.slide-leave-to
     transform: translate3d(100%, 0, 0)
+  .slidedown-enter-active,.slidedown-leave-active
+    transition: all 0.3s
+  .slidedown-enter,.slidedown-leave-to
+    transform: translate3d(0, -100%, 0)
+    opacity: 0%
   .route-line-detail
     position: fixed
     z-index: 100
     top: 0
     left: 0
-    bottom: 80px
+    bottom: 0
     right: 0
     background: $color-background
     .back
       position: absolute
+      z-index: 100
       top: 0
       left: 6px
       z-index: 50
+      background-color: $color-background
       .icon-corner-down-left
         display: block
         padding: 10px
@@ -147,6 +162,7 @@ export default {
         color: $color-theme
     .title
       position: absolute
+      z-index: 100
       top: 0
       left: 10%
       z-index: 40
@@ -156,14 +172,17 @@ export default {
       line-height: 40px
       font-size: $font-size-large
       color: $color-text
+      background-color: $color-background
     .start-or-end-wrapper
-      position: relative
+      z-index 100
+      position: absolute
       display: flex
       width: 100%
       height: 40px
-      padding-top: 40px;
+      top: 40px;
       transform-origin: top
       background-size: cover
+      background-color: $color-background
       .btn-translate
         background-color: $color-theme
         border-radius: 5px
@@ -173,43 +192,61 @@ export default {
           font-size: $font-size-large
           vertical-align: middle
           margin-right: 5px
+      .tag
+        padding: 5px
+        border-radius: 5px
+      .start
+        span.tag
+          background-color: #109c10
+      .end
+        span.tag
+          background-color red
     .start-or-end-wrapper > *
       flex: 1
       color: $color-text-ll
       font-size: $font-size-medium
       text-align: center
       align-self: center
-  .list
-    height: 100%
-    overflow: hidden
-    background: $color-highlight-background
-    .all-stations
-      background: $color-background
-      padding-bottom: 30px
-      .one-stataion
-        display: flex
-        align-items: center
-        padding: 20px 0 0 30px
-        .avatar
-          flex: 0 0 50px
-          width: 50px
-          height: 50px
-          border-radius: 50%
-        .name
-          flex: 1
-          margin-left: 20px
-          color: $color-text-l
-          font-size: $font-size-medium
-        .detail
-          flex: 0 0 100px
-          float: right
-          margin-left: 20px
-          color: $color-theme-d
-          font-size: $font-size-medium
-  .notify
-    font-size: $font-size-medium
-    color: $color-theme
-    background-color: $color-theme-d
-    padding: 9px 14px
-    height: 14px
+  .content-wrapper
+    bottom: 0;
+    position: absolute;
+    top: 80px;
+    right: 0;
+    left: 0;
+    .list
+      height: 100%
+      overflow: hidden
+      background: $color-highlight-background
+      .all-stations
+        background: $color-background
+        padding-bottom: 30px
+        .one-stataion
+          display: flex
+          align-items: center
+          padding: 20px 0 0 30px
+          .avatar
+            flex: 0 0 50px
+            width: 50px
+            height: 50px
+            border-radius: 50%
+          .name
+            flex: 1
+            margin-left: 20px
+            color: $color-text-l
+            font-size: $font-size-medium
+          .detail
+            flex: 0 0 100px
+            float: right
+            margin-left: 20px
+            color: $color-theme-d
+            font-size: $font-size-medium
+    .notify
+      position: absolute
+      font-size: $font-size-medium
+      color: $color-theme
+      background-color: $color-dialog-background-o
+      padding: 9px 14px
+      height: 28px
+      width: 100%
+      z-index 1
 </style>
