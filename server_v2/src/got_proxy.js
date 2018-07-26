cee234603a4a48a9797bbb2de5919fb543541a33
lanseria <zhangchao564265135@hotmail.com>
2018-07-26 17:03:17 +0800

    update server

--- server_v2/src/got_proxy.js
+++ server_v2/src/got_proxy.js
@@ -15,8 +15,7 @@ const gotConfig = {
     }
   }) : false,
   headers: {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-    'X-Requested-With': 'XMLHttpRequest'
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15G77 MicroMessenger/6.7.1 NetType/WIFI Language/en'
   }
 }
 
