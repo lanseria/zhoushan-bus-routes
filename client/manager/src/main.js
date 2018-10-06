a863b601b58561a0d19c555fb37413d34497ca5f
Lanseria <zhangchao564265135@hotmail.com>
2018-10-06 20:49:30 +0800

    no ts

--- client/manager/src/main.js
+++ client/manager/src/main.js
@@ -5,10 +5,10 @@ import store from './store'
 import './registerServiceWorker'
 import './plugins/element.js'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
