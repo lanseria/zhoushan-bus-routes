b10786edf114f306e07ee85dbe9e404b9c33bcff
Lanseria <zhangchao564265135@hotmail.com>
2019-07-24 23:56:02 +0800

    添加历史功能与修复加载问题

--- client/min-bus/dist/pages/my/index.js
+++ client/min-bus/dist/pages/my/index.js
@@ -3,11 +3,21 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _global = require('../../models/global.js');
+
+var _global2 = _interopRequireDefault(_global);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 exports.default = Page({
   data: {
     '__code__': {
       readme: ''
     }
+  },
+  clearHistoryLines: function clearHistoryLines() {
+    _global2.default.setVal('HistoryLines', []);
   }
 });
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4Lnd4cCJdLCJuYW1lcyI6WyJkYXRhIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFLRUEsUUFBTTtBQUFBO0FBQUE7QUFBQTtBQUFBIiwiZmlsZSI6ImluZGV4Lnd4cCIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcbiAgY29uZmlnOiB7XG4gICAgbmF2aWdhdGlvbkJhclRpdGxlVGV4dDogJ+aIkeeahCcsXG4gICAgdXNpbmdDb21wb25lbnRzOiB7fVxuICB9LFxuICBkYXRhOiB7fVxufSJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4Lnd4cCJdLCJuYW1lcyI6WyJkYXRhIiwiY2xlYXJIaXN0b3J5TGluZXMiLCJHbG9iYWwiLCJzZXRWYWwiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7O0FBU0VBLFFBQU07QUFBQTtBQUFBO0FBQUE7QUFBQSxHO0FBQ05DLG1CLCtCQUFxQjtBQUNuQkMscUJBQU9DLE1BQVAsQ0FBYyxjQUFkLEVBQStCLEVBQS9CO0FBQ0QiLCJmaWxlIjoiaW5kZXgud3hwIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IEdsb2JhbCBmcm9tIFwiLi4vLi4vbW9kZWxzL2dsb2JhbFwiO1xuZXhwb3J0IGRlZmF1bHQge1xuICBjb25maWc6IHtcbiAgICBuYXZpZ2F0aW9uQmFyVGl0bGVUZXh0OiAn5oiR55qEJyxcbiAgICB1c2luZ0NvbXBvbmVudHM6IHtcbiAgICAgICd3eGMtYnV0dG9uJzogJ0BtaW51aS93eGMtYnV0dG9uJyxcbiAgICAgICd3eGMtaWNvbic6ICdAbWludWkvd3hjLWljb24nXG4gICAgfVxuICB9LFxuICBkYXRhOiB7fSxcbiAgY2xlYXJIaXN0b3J5TGluZXMgKCkge1xuICAgIEdsb2JhbC5zZXRWYWwoJ0hpc3RvcnlMaW5lcycgLCBbXSlcbiAgfVxufSJdfQ==
