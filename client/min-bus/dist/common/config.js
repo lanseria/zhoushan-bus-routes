aaf06d325434d44aa025e66e7211b7457327fc29
lanseria <zhangchao564265135@hotmail.com>
2019-07-25 09:46:22 +0800

    优化打包

--- client/min-bus/dist/common/config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var HOST = exports.HOST = 'https://limonplayer.cn/jsonp/zhoushanbus';
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbmZpZy5qcyJdLCJuYW1lcyI6WyJIT1NUIl0sIm1hcHBpbmdzIjoiOzs7OztBQUFPLElBQU1BLHNCQUFPLDBDQUFiIiwiZmlsZSI6ImNvbmZpZy5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBIT1NUID0gJ2h0dHBzOi8vbGltb25wbGF5ZXIuY24vanNvbnAvemhvdXNoYW5idXMnIl19
