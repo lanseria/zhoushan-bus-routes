d68254632fedd6234e5e769741ab658dcc83164d
lanseria <zhangchao564265135@hotmail.com>
2020-04-27 16:41:59 +0800

    add appscript, remove serve_v3

--- server_v2/src/config.js
+++ server_v2/src/config.js
@@ -1,7 +1,9 @@
 const config = {
-  port: '7993',
-  gotHostPrefix: 'http://122.226.17.46:10251/WifiBusInterface/transfer',
-  root: './images',
-}
+  port: "7993",
+  gotHostPrefix: "http://122.226.17.46:10251/WifiBusInterface/transfer",
+  gotHostNewsPrefix:
+    "http://122.226.17.46:10251/WifiMobileLbs/mobile/skin-default/",
+  root: "./images",
+};
 
-exports = module.exports = config
+exports = module.exports = config;
--- server_v2/src/got_proxy.js
+++ server_v2/src/got_proxy.js
@@ -1,39 +1,47 @@
-const got = require('got');
-const tunnel = require('tunnel');
-const config = require('./config');
-const {
-  gotHostPrefix
-} = config;
-const isDevelopmentProxy = process.env.NODE_ENV === 'development-proxy';
-console.log(isDevelopmentProxy);
+const got = require("got");
+const tunnel = require("tunnel");
+const config = require("./config");
+const { gotHostPrefix, gotHostNewsPrefix } = config;
+const isDevelopmentProxy = process.env.NODE_ENV === "development-proxy";
 
 const gotConfig = {
-  agent: isDevelopmentProxy ? tunnel.httpOverHttp({
-    proxy: {
-      host: 'localhost',
-      port: 8888
-    }
-  }) : false
-}
+  agent: isDevelopmentProxy
+    ? tunnel.httpOverHttp({
+        proxy: {
+          host: "localhost",
+          port: 8888,
+        },
+      })
+    : false,
+};
 
 exports.getFromServer = async function (apiUrl, query) {
   const url = gotHostPrefix + apiUrl;
   const response = await got.get(url, {
     ...gotConfig,
-    query: query
-  })
+    query: query,
+  });
   return response;
-}
+};
+
+exports.getNewsFromServer = async function (apiUrl, query) {
+  const url = gotHostNewsPrefix + apiUrl;
+  const response = await got.get(url, {
+    ...gotConfig,
+    query: query,
+  });
+  return response;
+};
 
 exports.postFromServer = async function (apiUrl, querystring) {
   const url = gotHostPrefix + apiUrl;
   const response = await got.post(url, {
     ...gotConfig,
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'User-Agent': `BusXing/1.3.0 (iPhone; iOS 11.4.1; Scale/3.00)`
+      "Content-Type": "application/x-www-form-urlencoded",
+      "User-Agent": `BusXing/1.3.0 (iPhone; iOS 11.4.1; Scale/3.00)`,
     },
     body: querystring,
-  })
+  });
   return response;
-}
+};
