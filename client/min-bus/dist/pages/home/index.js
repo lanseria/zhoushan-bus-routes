a19cbe84599f1e16b774d99da33a07c08d951cf2
Lanseria <zhangchao564265135@hotmail.com>
2020-08-25 15:56:13 +0800

    修复接口

--- client/min-bus/dist/pages/home/index.js
+++ client/min-bus/dist/pages/home/index.js
@@ -39,7 +39,7 @@ exports.default = Page({
   /** note: 在 wxp 文件或者页面文件中请去掉 methods 包装 */
   onLoad: function onLoad() {
     this.saveData();
-    this.getNearLine();
+    // this.getNearLine();
   },
   onShow: function onShow() {
     var hl = _global2.default.getVal('HistoryLines') || [];
--- server_v2/src/app.js
+++ server_v2/src/app.js
@@ -1,36 +1,20 @@
-const Koa = require('koa');
-const path = require('path');
-const static = require('koa-static');
-const jsonp = require('koa-jsonp');
-const mongoose = require('mongoose');
-const router = require('./router');
-const config = require('./config');
-const {
-  port,
-  root,
-} = config;
+const Koa = require("koa");
+const path = require("path");
+const static = require("koa-static");
+const jsonp = require("koa-jsonp");
+const router = require("./router");
+const config = require("./config");
+const { port, root } = config;
 
 const app = new Koa();
-const db = mongoose.connection;
 
-app.use(static(
-  path.join( __dirname,  root)
-));
+app.use(static(path.join(__dirname, root)));
 
 app.proxy = true;
 
 app.use(jsonp());
 app.use(router.routes()).use(router.allowedMethods());
 
-
-mongoose.connect('mongodb://localhost:27017/test', {
-  useNewUrlParser: true
-});
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('mongodb has been connected!');
-})
-
 module.exports = app.listen(port, () => {
-  console.log(`app is listening at port ${port}\n舟山公交API服务Version_2`)
-})
+  console.log(`app is listening at port ${port}\n舟山公交API服务Version_2`);
+});
